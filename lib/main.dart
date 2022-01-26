c54bf9d3c97da20f60cf01e5c11cbe2a0f2d47b4
Fylyp Berzenin <FBerzenin@Amerilife.com>
2022-01-26 18:53:58 +0200

    init commit1

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,55 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  int questionNumber = 0;
+  int correctAnswer = 0;
+  int wrongAnswer = 0;
   List<Widget> scoreKipper = [];
+  List<Questions> questions = [
+    new Questions('You can lead a cow down stairs but not up stairs.', false),
+    new Questions(
+        'Approximately one quarter of human bones are in the feet.', true),
+    new Questions('A slug\'s blood is green.', true),
+    Questions('End you have a ', true)
+  ];
+
+  Icon checkingCorrectAnswer(bool answer) {
+    if (questions.elementAt(questionNumber).answer != answer) {
+      wrongAnswer++;
+      return Icon(
+        Icons.close,
+        color: Colors.red,
+      );
+    } else {
+      correctAnswer++;
+      return Icon(
+        Icons.check,
+        color: Colors.green,
+      );
+    }
+  }
+
+  Text returnQuastion() {
+    return Text(
+      questions.elementAt(questionNumber).text,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 25.0,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  FlatButton returnRestartButton() {
+    return FlatButton(
+      child: Text('Restart'),
+      textColor: Colors.white,
+      color: Colors.green,
+      onPressed: () {},
+    );
+  }
+
+  bool checkTheEnd() => questionNumber == (questions.length - 1);
 
   @override
   Widget build(BuildContext context) {
@@ -37,16 +85,7 @@ class _QuizPageState extends State<QuizPage> {
           flex: 5,
           child: Padding(
             padding: EdgeInsets.all(10.0),
-            child: Center(
-              child: Text(
-                'This is where the question text will go.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.white,
-                ),
-              ),
-            ),
+            child: Center(child: returnRestartButton()),
           ),
         ),
         Expanded(
@@ -64,12 +103,11 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  scoreKipper.add(
-                    Icon(
-                      Icons.close,
-                      color: Colors.red,
-                    ),
-                  );
+                  if (!checkTheEnd()) {
+                    scoreKipper.add(checkingCorrectAnswer(true));
+                    questionNumber++;
+                  } else
+                    null;
                 });
               },
             ),
@@ -88,7 +126,13 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                setState(() {
+                  if (!checkTheEnd()) {
+                    scoreKipper.add(checkingCorrectAnswer(false));
+                    questionNumber++;
+                  } else
+                    null;
+                });
               },
             ),
           ),
@@ -96,6 +140,16 @@ class _QuizPageState extends State<QuizPage> {
         Row(children: scoreKipper),
       ],
     );
+  }
+}
+
+class Questions {
+  String text = "";
+  bool answer = false;
+
+  Questions(String text, bool answer) {
+    this.text = text;
+    this.answer = answer;
   }
 }
 /*
